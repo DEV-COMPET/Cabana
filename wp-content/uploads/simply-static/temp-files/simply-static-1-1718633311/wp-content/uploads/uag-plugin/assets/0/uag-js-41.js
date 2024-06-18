2fced9b3c4f9884c7b9c3dff2bf74c53ca3147af
Arthur <dss.arthur0@gmail.com>
2024-06-18 10:28:49 -0300

    Novas midias e formulario do site

---
+++ wp-content/uploads/simply-static/temp-files/simply-static-1-1718633311/wp-content/uploads/uag-plugin/assets/0/uag-js-41.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function(){ window.addEventListener("DOMContentLoaded", function(){
+	UAGBTimelineClasses( {"block_id":"cb8ae62e","timelinAlignment":"center","timelinAlignmentTablet":"left","timelinAlignmentMobile":"left"}, '.uagb-block-cb8ae62e' );
+});
+window.addEventListener("resize", function(){
+	UAGBTimelineClasses( {"block_id":"cb8ae62e","timelinAlignment":"center","timelinAlignmentTablet":"left","timelinAlignmentMobile":"left"}, '.uagb-block-cb8ae62e' );
+});
+ });
